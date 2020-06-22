2f8906e46e8642702155a58648ac3de94c4f5d59
Giovanni Sanchez <dgiovanni15@hotmail.com>
2020-06-21 23:23:58 -0500

    Fix consumer route

--- consumer/src/http_api/apps/consumer/routes.py
+++ consumer/src/http_api/apps/consumer/routes.py
@@ -7,9 +7,9 @@ from src.application.use_cases.consumer import ConsumerUse
 consumer = Blueprint("consumer", __name__)
 
 
-@consumer.route("/", methods=["POST"])
+@consumer.route("/", methods=["GET"])
 def start_consumer():
-    if request.method == "POST":
+    if request.method == "GET":
         data = request.get_json()
         ConsumerUse.receiver(data)
         return jsonify({"message": "it works"}), 200
